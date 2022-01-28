923cf2206a35049753ffe27104acd5cf8c783cf4
Melvin A <melvinalmonte@gmail.com>
2022-01-27 23:18:49 -0500

    initial commit

--- nodeReq.js
+++ nodeReq.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
-const pythonServer = async () => {
+const http = require("http");
+const pythonServerAxios = async () => {
   const res = await axios({
     url: "http://localhost:8080",
     method: "GET",
@@ -11,8 +12,35 @@ const pythonServer = async () => {
   return res.data;
 };
 
-const pythonTester = async () => {
-  const myRes = await pythonServer();
-  console.log(myRes);
+const pythonServiceNative = (callback) => {
+  const options = {
+    host: "localhost",
+    path: "/?bing=bong&ping=pong",
+    port: 8080,
+  };
+  const req = http.request(options, (res) => {
+    res.setEncoding("utf-8");
+
+    res.on("data", (chunk) => {
+      callback(JSON.parse(chunk));
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+
+  req.end();
 };
-pythonTester();
+
+const pythonTesterAxios = async () => {
+  const myRes = await pythonServerAxios();
+  console.log("AXIOS: ", myRes);
+};
+
+const pythonTesterNative = (myRes) => {
+  console.log("NATIVE: ", myRes);
+};
+
+pythonTesterAxios();
+pythonServiceNative(res => pythonTesterNative(res))
